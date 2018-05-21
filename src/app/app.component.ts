5ee3aba0fc188869c31283ff2b452f7115ea62f1
invocamanman <jesus.ligero@yahoo.es>
2018-05-21 13:47:30 +0200

    sidemenu MOLA mes :D :D ( y es pasa el username)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import {Events, Nav, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -17,16 +17,24 @@ import {MessagesPage} from '../pages/messages/messages'
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-
+  username:String;
   rootPage:any = HomePage;
 
   pages: Array<{title: string, component: any, icon: string}>;
 
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public events: Events) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.username = localStorage.getItem('username');
+
+      events.subscribe('user:created', (username) => {
+        // user and time are the same arguments passed in `events.publish(user, time)`
+        this.username=username;
+      });
+
       statusBar.styleDefault();
       splashScreen.hide();
       this.pages = [
@@ -40,6 +48,7 @@ export class MyApp {
       ];
     });
   }
+
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
