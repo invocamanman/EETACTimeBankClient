bc13b1b236279ced872cd59c01b02b99b7cf9a88
invocamanman <jesus.ligero@yahoo.es>
2018-05-21 12:59:47 +0200

    sidemenu MOLA mes :D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class MyApp {
 
   rootPage:any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any, icon: string}>;
 
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
@@ -30,12 +30,12 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
       this.pages = [
-        { title: 'Pagina Principal', component: MainpagePage},
-        { title: 'Perfil', component: ProfilePage},
-        { title: 'Missatges' , component: MessagesPage},
-        { title: 'Banc' , component: BancPage},
-        { title: 'Peticions' , component: ActivityRequestPage},
-        { title: 'Exit', component: HomePage }
+        { title: 'Pagina Principal', component: MainpagePage, icon: 'home'},
+        { title: 'Perfil', component: ProfilePage, icon: 'contact'},
+        { title: 'Missatges' , component: MessagesPage, icon: 'mail'},
+        { title: 'Banc' , component: BancPage, icon: 'cash'},
+        { title: 'Peticions' , component: ActivityRequestPage, icon: 'notifications'},
+        { title: 'Exit', component: HomePage, icon: 'exit' }
 
       ];
     });
