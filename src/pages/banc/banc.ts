f6a9a543e0da32f0ee1b63f529c9ae3055f87418
IfJosean <josean7jkami@gmail.com>
2018-05-25 00:26:45 +0200

    banc ionic

--- src/pages/banc/banc.ts
+++ src/pages/banc/banc.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {ActivityRequest} from "../../models/activityRequest.model";
+import {ActivityRequestService} from "../../providers/activity-request-service/activity-request.service";
+import {UserServiceProvider} from "../../providers/user-service/user-service";
 
 /**
  * Generated class for the BancPage page.
@@ -14,12 +17,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'banc.html',
 })
 export class BancPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  SearchDone = false;
+  myActivitiesList: Array<ActivityRequest> = [];
+  theirActivitiesList: Array<ActivityRequest> = [];
+  user = {username: 'Usuario', wallet: 'Wallet'};
+  constructor(public navCtrl: NavController, public navParams: NavParams, private activityRequestService: ActivityRequestService, private userService: UserServiceProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BancPage');
   }
 
+  getDoneActivities() {
+    const id = localStorage.getItem('userId');
+    this.activityRequestService.getMyPetitions(id).subscribe(
+      data => {
+        const da: any = data;
+        this.myActivitiesList = da;
+        this.SearchDone = true;
+        console.log(this.myActivitiesList);
+      }
+    );
+    this.activityRequestService.getTheirPetitions(id).subscribe(
+      data => {
+        const da2: any = data;
+        this.theirActivitiesList = da2;
+        this.SearchDone = true;
+        console.log(this.theirActivitiesList);
+      }
+    );
+  }
+  getWallet() {
+    const id = localStorage.getItem('userId');
+    this.userService.getUserWallet(id).subscribe(
+      data => {
+        this.user = data;
+        console.log(data);
+      },
+      data => {
+        console.error(data);
+      });
+  }
 }
